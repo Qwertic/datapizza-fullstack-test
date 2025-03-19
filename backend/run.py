23e000764298c492c06cc8c1d52f72339bceb0f4
Qwertic <vodaregi@gmail.com>
2025-03-19 18:30:20 +0100

    🌮

--- backend/run.py
+++ backend/run.py
@@ -2,6 +2,5 @@ import uvicorn
 import os
 
 if __name__ == "__main__":
-
     reload = os.environ.get("ENVIRONMENT") != "production"
     uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=reload)
